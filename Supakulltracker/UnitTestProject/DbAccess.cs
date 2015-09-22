83bea54882489891a2180e299bf326169ed2f28a
pavelsokolovan <pavel.sokolov.an@gmail.com>
2015-09-22 16:27:09 +0300

    Add Unit Test class to check access to DB

---
+++ Supakulltracker/UnitTestProject/DbAccess.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHibernate.Cfg;
+using UnitTestProject.Class;
+using NHibernate.Tool.hbm2ddl;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class DBAccess
+    {
+        [TestMethod]
+        public void CanGenerateSchema()
+        {
+            var nHibernateConfiguration = new Configuration();
+            nHibernateConfiguration.Configure();
+            nHibernateConfiguration.AddAssembly(typeof(ItemForTesting).Assembly);
+            new SchemaExport(nHibernateConfiguration).Execute(false, true, false);
+        }
+    }
+}
