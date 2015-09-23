131e385cc30afe213a18141527ef4da7506e701d
pavelsokolovan <pavel.sokolov.an@gmail.com>
2015-09-23 11:35:59 +0300

    Add service UserProvider to find user in USER table in DB

---
+++ Supakulltracker/SupakullTrackerServices/UserProvider.asmx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using SupakullTrackerServices.Class;
+using NHibernate.Linq;
+using NHibernate.Criterion;
+
+namespace SupakullTrackerServices
+{
+    /// <summary>
+    /// Summary description for UserProvider
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class UserProvider : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public User Find(string userLogin, string userPinCode)
+        {
+            var sessionFactory = new NhibernateSessionFactory().CreateSessionFactory();
+
+            using (var session = sessionFactory.OpenSession())
+            {
+                User user = session
+                    .CreateCriteria(typeof(User))
+                    .Add(Restrictions.Eq("UserLogin", userLogin))
+                    .Add(Restrictions.Eq("UserPinCode", userPinCode))
+                    .UniqueResult<User>();
+                return user;
+            }
+        }
+    }
+}
