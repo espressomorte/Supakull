645ac4423d1eeb6e53e8511169faa98c3a84fe3f
Денис Руденко <rudenko.denis@hotmail.com>
2015-10-08 00:24:10 +0300

    Tabs with details

--- Supakulltracker/Supakulltracker/MainForm.cs
+++ Supakulltracker/Supakulltracker/MainForm.cs
@@ -13,8 +13,8 @@ namespace Supakulltracker
     public partial class MainForm : Form
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        IssueService.TaskMainDTO[] Tasks;
 
-       
         public MainForm()
         {
             InitializeComponent();
@@ -22,22 +22,29 @@ namespace Supakulltracker
 
 
         private void Form1_Load(object sender, EventArgs e)
-        {            
+        {
             IssueService.GetTrackerServicesSoapClient client = new IssueService.GetTrackerServicesSoapClient();
             client.StoreSources();
-            Board.DataSource = client.GetAllTasks();
+            Tasks = client.GetAllTasks();
+            Board.DataSource = Tasks;
         }
 
         private void Board_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            NewTab();
+            NewTab(e.RowIndex);
         }
 
-        private void NewTab()
+        private void NewTab(int index)
         {
-            string title = "TabPage " + (taskDetailTabControl.TabCount + 1).ToString();
+            IssueService.TaskMainDTO task = Tasks[index];
+            string title = (task.TaskID).ToString();
             TabPage newTabPage = new TabPage(title);
+            var detail = new DetailPanel();
+            detail.Dock = DockStyle.Fill;
+            detail.Fill(task);
+            newTabPage.Controls.Add(detail);
             taskDetailTabControl.TabPages.Add(newTabPage);
+            taskDetailTabControl.SelectTab(taskDetailTabControl.TabCount-1);
         }
     }
 }
