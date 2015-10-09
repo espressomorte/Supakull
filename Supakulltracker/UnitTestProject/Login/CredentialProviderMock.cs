4a2362203fa5dc5083a137d86e35b6a2f7a90136
pavelsokolovan <pavel.sokolov.an@gmail.com>
2015-10-09 16:16:35 +0300

    Re-add Unit Tests for testing LoginProvider class

---
+++ Supakulltracker/UnitTestProject/Login/CredentialProviderMock.cs
@@ -0,0 +1,29 @@
+﻿using Supakulltracker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Login
+{
+    class CredentialProviderMock : ICredentialsProvider
+    {
+        private CredentialInfo credentialsInfo;
+
+        public CredentialProviderMock()
+        {
+            credentialsInfo = new CredentialInfo("SomeUserLogin");
+        }
+
+        public CredentialProviderMock(CredentialInfo credentialsInfo)
+        {
+            this.credentialsInfo = credentialsInfo;
+        }
+
+        public CredentialInfo GetCredentialsInfo(string message)
+        {
+            return credentialsInfo;
+        }
+    }
+}
---
+++ Supakulltracker/UnitTestProject/Login/LoginProviderTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Supakulltracker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Login
+{
+    [TestFixture]
+    class LoginProviderTest
+    {
+        [Test]
+        public void LoginUserMethodReturnsTrueWhenUserAuthorized()
+        {
+            ICredentialsProvider credentialsProviderMock = new CredentialProviderMock();
+            IAuthorizer authorizerMock = new AuthorizerMockReturnTrue();
+            LoginProvider loginProvider = new LoginProvider();
+
+            bool actualLoginResult = loginProvider.LoginUser(credentialsProviderMock, authorizerMock);
+
+            Assert.IsTrue(actualLoginResult, "LoginUser method returns incorrect value");
+        }
+
+        [Test]
+        public void LoginUserMethodReturnsFalseWhenUserNotAuthorized()
+        {
+            ICredentialsProvider credentialsProviderMock = new CredentialProviderMock(null);
+            IAuthorizer authorizerMock = new AuthorizerMockReturnTrue();
+            LoginProvider loginProvider = new LoginProvider();
+
+            bool actualLoginResult = loginProvider.LoginUser(credentialsProviderMock, authorizerMock);
+
+            Assert.IsFalse(actualLoginResult, "LoginUser method returns incorrect value");
+        }
+    }
+}
