bf59da30fa639aa6427255e7890092787607dcb8
Yura-Fedorov <dmdofer@gmail.com>
2015-10-21 17:06:10 +0300

    Working mapping for settings

--- Supakulltracker/SupakullTrackerServices/DAO/SettingsDAO/ServiceAccountDAO.cs
+++ Supakulltracker/SupakullTrackerServices/DAO/SettingsDAO/ServiceAccountDAO.cs
@@ -10,22 +10,16 @@ namespace SupakullTrackerServices
     {
         public ServiceAccountDAO()
         {
-            this.FirstToken = new Dictionary<String, String>();
-            this.SecondToken = new Dictionary<String, String>();
-            this.ThirdToken = new Dictionary<String, String>();
-            this.FourthToken = new Dictionary<String, String>();
-            this.FifthToken = new Dictionary<String, String>();
+            this.Tokens = new List<TokenDAO>();
             this.MappingTemplates = new List<TemplateDAO>();
         }
         public virtual Int32 ServiceAccountId { get; set; }
         public virtual String ServiceAccountName { get; set; }
         public virtual Sources Source { get; set; }
         public virtual IList<TemplateDAO> MappingTemplates { get; set; }
-        public virtual IDictionary<String, String> FirstToken { get; set; }
-        public virtual IDictionary<String, String> SecondToken { get; set; }
-        public virtual IDictionary<String, String> ThirdToken { get; set; }
-        public virtual IDictionary<String, String> FourthToken { get; set; }
-        public virtual IDictionary<String, String> FifthToken { get; set; }
+        public virtual IList<TokenDAO> Tokens { get; set; }
+
+
 
 
     }
---
+++ Supakulltracker/SupakullTrackerServices/DAO/SettingsDAO/TokenDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SupakullTrackerServices
+{
+    public class TokenDAO
+    {
+        public TokenDAO()
+        {
+            this.Token = new Dictionary<String, String>();
+        }
+        public virtual Int32 TokeneId { get; set; }
+        public virtual String TokeneName { get; set; }
+        public virtual IDictionary<String, String> Token { get; set; }
+    }
+}
